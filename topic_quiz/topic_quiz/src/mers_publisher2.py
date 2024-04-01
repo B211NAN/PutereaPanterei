bc94a279c9c88e3fe1d5ec6a231d7d69de156ba6
OEM Configuration (temporary user) <alexnanmihai@gmail.com>
2024-04-01 17:09:59 +0300

    aici e cel bun

--- topic_quiz/topic_quiz/src/mers_publisher2.py
+++ topic_quiz/topic_quiz/src/mers_publisher2.py
@@ -8,21 +8,44 @@ from geometry_msgs.msg import Twist
 def callback(msg):  
     print (msg.ranges[0])
     if msg.ranges[0] < 1:
-      #stop turn right
+      #stop turn left
       turn.linear.x = 0
       turn.angular.z = 0
       pub.publish(turn)
-      time.sleep(0.5)
-      turn.angular.z = 0.5
+      time.sleep(2)
+      
+      for i in range(4):
+      	turn.angular.z = 0.2
+      	pub.publish(turn) 
+      	time.sleep(2)
+      	i+=1
+      
+      turn.linear.x = 0.2
+      turn.angular.z = 0
       pub.publish(turn)
-      time.sleep(0.5)
-    #mers dreapta
+      time.sleep(2)
+      
+      turn.linear.x = 0
+      turn.angular.z = 0
+      pub.publish(turn)
+      time.sleep(2)
+      
+      turn.linear.x = 0.2
+      turn.angular.z = 0
+      pub.publish(turn)
+      time.sleep(2)
+      
+      #for i in range(4):
+      	#turn.angular.z = -0.2
+      	#turn.linear.x = 0
+      	#pub.publish(turn) 
+      	#time.sleep(2)
+      	#i+=1
+      
+    #mers inainte
+    turn.linear.x = 0.2
     pub.publish(turn)
-    turn.linear.x = 0.5
-    pub.publish(turn)
-
-
-
+    
 rospy.init_node('scan_publisher')
 pub = rospy.Publisher('/cmd_vel', Twist, queue_size = 1) 
 sub = rospy.Subscriber('/scan', LaserScan, callback) 
@@ -31,4 +54,3 @@ rate = rospy.Rate(2)
 turn = Twist()
 
 rospy.spin()
-
